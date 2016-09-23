b2fe869d48342be209f19329eccdb3a9000a8aeb
Michael Carlisle <michael.carlisle@akqa.com>
2016-09-23 19:30:12 +0100

    Initial checkin.

---
+++ src/SaintModeCache.Net.Tests/SaintModeCacheTests.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Runtime.Caching;
+using System.Threading;
+using NUnit.Framework;
+
+namespace SaintModeCaching.Tests
+{
+    [TestFixture]
+    public class SaintModeCacheTests
+    {
+        [Test]
+        public void WhenObjectNotInCacheThenReturnResult()
+        {
+            // arrange
+            var expectedResult = "value";
+            var cacheKey = "key";
+            var cache = new SaintModeCache();
+
+            // act
+            var cacheValue = cache.GetOrUpdate(cacheKey, key => expectedResult, ObjectCache.InfiniteAbsoluteExpiration);
+
+            // assert
+            Assert.That(cacheValue, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        public void WhenObjectInCacheThenReturnResult()
+        {
+            // arrange
+            var expectedResult = "expectedResult";
+            var unexpectedResult = "unexpectedResult";
+            var cacheKey = "key";
+            var cache = new SaintModeCache();
+            cache.GetOrUpdate(cacheKey, key => expectedResult, ObjectCache.InfiniteAbsoluteExpiration);
+
+            // act
+            var cacheValue = cache.GetOrUpdate(cacheKey, key => unexpectedResult, ObjectCache.InfiniteAbsoluteExpiration);
+            cache.GetOrUpdate(cacheKey, key => expectedResult, ObjectCache.InfiniteAbsoluteExpiration);
+
+            // assert
+            Assert.That(cacheValue, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        public void WhenObjectExpiredThenReturnOldResultAndTriggerRefresh()
+        {
+            // arrange
+            var expectedResult = "expectedResult";
+            var unexpectedResult = "unexpectedResult";
+            var cacheKey = "key";
+            var triggerUpdate = false;
+            var cache = new SaintModeCache();
+            var policy = new CacheItemPolicy {AbsoluteExpiration = DateTime.UtcNow.AddMilliseconds(10)};
+            cache.GetOrUpdate(cacheKey, key => expectedResult, policy);
+            Thread.Sleep(20);
+            var ewh = new EventWaitHandle(false, EventResetMode.ManualReset);
+
+            // act
+            var cacheValue = cache.GetOrUpdate(cacheKey, key =>
+            {
+                triggerUpdate = true;
+                ewh.Set();
+                return unexpectedResult;
+            }, policy);
+
+            // assert
+            Assert.That(cacheValue, Is.EqualTo(expectedResult));
+            ewh.WaitOne();
+            Assert.That(triggerUpdate, Is.True);
+
+        }
+
+        [Test]
+        public void WhenObjectExpiresThenAfterRefreshReturnUpdatedValue()
+        {
+            // arrange
+            var expectedResult = "expectedResult";
+            var unexpectedResult = "unexpectedResult";
+            var cacheKey = "key";
+            var cache = new SaintModeCache();
+            var policy = new CacheItemPolicy { AbsoluteExpiration = DateTime.UtcNow.AddMilliseconds(10) };
+            cache.GetOrUpdate(cacheKey, key => unexpectedResult, policy);
+            Thread.Sleep(20);
+            var ewh = new EventWaitHandle(false, EventResetMode.ManualReset);
+            cache.GetOrUpdate(cacheKey, key =>
+            {
+                ewh.Set();
+                return expectedResult;
+            }, ObjectCache.InfiniteAbsoluteExpiration);
+
+            // act
+            ewh.WaitOne();
+            var cacheValue = cache.GetOrUpdate(cacheKey, key => expectedResult, policy);
+
+            // assert
+            Assert.That(cacheValue, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        public void WhenNoCacheMultipleThreadsRequestValueThenOnlyOneCreatesValue()
+        {
+            // arrange
+            var expectedResult = "expectedResult";
+            var cacheKey = "key";
+            var cache = new SaintModeCache();
+            int counter = 0;
+            var ewh = new EventWaitHandle(false, EventResetMode.ManualReset);
+
+            // act //assert
+            for (var i=0; i< 100000; i++)
+            {
+                Func<object> testCache = () => cache.GetOrUpdate(cacheKey, key =>
+                {
+                    ewh.WaitOne();
+                    Interlocked.Increment(ref counter);
+                    return expectedResult;
+                });
+
+                testCache.BeginInvoke(re =>
+                {
+                    var result = testCache.EndInvoke(re);
+                    Assert.That(result, Is.EqualTo(expectedResult));
+                    Assert.That(counter, Is.EqualTo(1));
+                }, testCache);
+            }
+
+            ewh.Set();
+        }
+
+        [Test]
+        public void WhenCacheExpiresMultipleThreadsRequestValueThenOnlyOneCreatesValue()
+        {
+            // arrange
+            var expectedResult = "expectedResult";
+            var cacheKey = "key";
+            var cache = new SaintModeCache();
+            var policy = new CacheItemPolicy { AbsoluteExpiration = DateTime.UtcNow.AddMilliseconds(10) };
+            cache.GetOrUpdate(cacheKey, key => expectedResult, policy);
+            Thread.Sleep(20);
+            int counter = 0;
+            var ewh = new EventWaitHandle(false, EventResetMode.ManualReset);
+
+            // act //assert
+            for (var i = 0; i < 100000; i++)
+            {
+                Func<object> testCache = () =>
+                {
+                    ewh.WaitOne();
+                    return cache.GetOrUpdate(cacheKey, key =>
+                    {
+                        Interlocked.Increment(ref counter);
+                        return expectedResult;
+                    }, policy);
+                };
+
+                testCache.BeginInvoke(re =>
+                {
+                    var result = testCache.EndInvoke(re);
+                    Assert.That(result, Is.EqualTo(expectedResult));
+                    Assert.That(counter, Is.LessThanOrEqualTo(1));
+                }, testCache);
+            }
+
+            ewh.Set();
+        }
+    }
+}
---
+++ src/SaintModeCache.Net/SaintModeCache.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Runtime.Caching;
+using System.Threading;
+
+namespace SaintModeCaching
+{
+    public class SaintModeCache
+    {
+        private const string CacheKeyPrefix = "_#shadow_";
+        private readonly MemoryCache storeCache;
+        private readonly MemoryCache shadowCache;
+
+        public SaintModeCache()
+            : this(new MemoryCache("SaintModeCache", null))
+        {
+        }
+
+        public SaintModeCache(MemoryCache memoryCache)
+        {
+            this.storeCache = memoryCache;
+            this.shadowCache = new MemoryCache("SaintModeCacheShadow", null);
+        }
+
+        public TCacheItem GetOrUpdate<TCacheItem>(string key, Func<string, TCacheItem> fetchDelegate) 
+            where TCacheItem : class
+        {
+            var policy = new CacheItemPolicy {AbsoluteExpiration = ObjectCache.InfiniteAbsoluteExpiration};
+            return GetOrUpdate(key, fetchDelegate, policy);
+        }
+
+        public TCacheItem GetOrUpdate<TCacheItem>(string key, Func<string, TCacheItem> fetchDelegate, DateTimeOffset offset)
+            where TCacheItem : class
+        {
+            return GetOrUpdate(key, fetchDelegate, new CacheItemPolicy {AbsoluteExpiration = offset});
+        }
+
+        public TCacheItem GetOrUpdate<TCacheItem>(string key, Func<string, TCacheItem> fetchDelegate,
+            CacheItemPolicy cachePolicy) where TCacheItem : class
+        {
+            var shadowKey = GetShadowKey(key);
+            var shadowCacheItem = this.shadowCache.Get(shadowKey);
+            var item = this.storeCache.Get(key);
+            if (shadowCacheItem == null && item != null)
+            {
+                Action<string, Func<string, object>, CacheItemPolicy> onAsyncUpdateCache = OnAsyncUpdateCache;
+                onAsyncUpdateCache.BeginInvoke(key, fetchDelegate, cachePolicy, null, null);
+            }
+
+            if (item != null)
+            {
+                return item as TCacheItem;
+            }
+
+            lock (key)
+            {
+                item = this.storeCache[key];
+                if (item != null)
+                {
+                    return item as TCacheItem;
+                }
+
+                item = OnUpdateCache(key, fetchDelegate, cachePolicy);
+            }
+
+            return item as TCacheItem;
+        }
+
+        private void OnAsyncUpdateCache(string key, Func<string, object> func, CacheItemPolicy cachePolicy)
+        {
+            var shadowKey = GetShadowKey(key);
+            var item = this.shadowCache.Get(shadowKey);
+            if (item != null)
+            {
+                return;
+            }
+
+            if (!Monitor.TryEnter(shadowKey))
+            {
+                return;
+            }
+
+            lock (shadowKey)
+            {
+                item = this.shadowCache.Get(shadowKey);
+                if (item != null)
+                {
+                    return;
+                }
+
+                OnUpdateCache(key, func, cachePolicy);
+            }
+        }
+
+        private object OnUpdateCache(string key, Func<string, object> func, CacheItemPolicy cachePolicy)
+        {
+            var item = func(key);
+            this.storeCache.Set(key, item, ObjectCache.InfiniteAbsoluteExpiration);
+            this.shadowCache.Set(GetShadowKey(key), DateTime.UtcNow, cachePolicy);
+            return item;
+        }
+
+
+        private static string GetShadowKey(string key)
+        {
+            return string.Concat(CacheKeyPrefix, key);
+        }
+    }
+}
