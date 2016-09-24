83eb5e498f703a86e9f449d71d72c6c4d472457a
Michael Carlisle <michael.carlisle@akqa.com>
2016-09-24 17:01:04 +0100

    Release 1.0.1

--- src/SaintModeCache.Net/Properties/AssemblyInfo.cs
+++ src/SaintModeCache.Net/Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1")]
 [assembly: AssemblyTitle("XSaintModeCache.Net")]
 [assembly: AssemblyProduct("TheCodeKing.SaintModeCache.Net")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
